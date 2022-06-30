b8f7f7cad8c9b39f665b5b6e971006a241da71ab
kyle <bartscholar@gmail.com>
2022-06-29 19:52:49 -0500

    added dashboard

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -7,13 +7,11 @@ const packageJson = require('../package.json');
 const devConfig = {
     mode: 'development',
     output: {
-        publicPath: 'http://localhost:8081/',
+        publicPath: 'http://localhost:8081/'
     },
     devServer: {
         port: 8081,
-        historyApiFallback: {
-            index: 'index.html',
-        },
+        historyApiFallback: true,
     },
     plugins: [
         new ModuleFederationPlugin({
